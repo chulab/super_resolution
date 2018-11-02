bc97350be35073fa180f785671c93c8cf9824797
Noahyt <NoahToyonaga@gmail.com>
2018-11-02 12:41:21 -0700

    Implement new `utils` module.
    CONTEXT: We need methods for generating discrete gaussians to model response behavior on the discrete grid.
    
    SCOPT: Create `utils` module with function for discrete gaussians.

---
+++ simulation/utils_test.py
@@ -0,0 +1,12 @@
+"""Test for `utils.py`"""
+
+import numpy as np
+from simulation import utils
+import unittest
+
+
+class utilsTest(unittest.TestCase):
+  pass
+
+if __name__ == "__main__":
+  unittest.main()
