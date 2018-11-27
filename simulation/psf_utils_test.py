b252d4cabbd5a4037d8af38596f880d3fe2fbe2d
Noahyt <NoahToyonaga@gmail.com>
2018-11-26 16:00:32 -0800

    Implement new `psf_utils` module.
    CONTEXT: `psf_utils` contains functions for preparing psfs for use in observation simulations.
    
    SCOPE: `psf_utils` contains `to_filter` which takes a list of psfs and builds a filter. This can be used, for example, when making the lateral and axial psfs.

---
+++ simulation/psf_utils_test.py
@@ -0,0 +1,48 @@
+"""Tests for `psf_utils`."""
+
+import unittest
+import numpy as np
+
+from simulation import psf_utils
+
+
+class PsfUtilsTest(unittest.TestCase):
+
+  def testSameMode(self):
+    psfs = [np.random.rand(10) for _ in range(5)]
+    filter = psf_utils.to_filter(psfs, 'FROM_SAME')
+    self.assertEqual(filter.shape, (10, 5, 5))
+    for in_channel_iter in range(5):
+      for filter_iter in range(5):
+        if in_channel_iter == filter_iter:
+          np.testing.assert_allclose(
+            filter[:, in_channel_iter, filter_iter], psfs[in_channel_iter])
+        else:
+          np.testing.assert_equal(
+            filter[:, in_channel_iter, filter_iter], np.zeros_like(psfs[0]))
+
+  def testFromSingleMode(self):
+    psfs = [np.random.rand(10) for _ in range(5)]
+    filter = psf_utils.to_filter(psfs, 'FROM_SINGLE')
+    self.assertEqual(filter.shape, (10, 1, 5))
+    for filter_iter in range(5):
+      np.testing.assert_allclose(filter[:, 0, filter_iter], psfs[filter_iter])
+
+  def testBadMode(self):
+    mode = "NOT_A_MODE"
+    with self.assertRaisesRegex(ValueError, "`mode` must be one of"):
+      psf_utils.to_filter([np.ones(1)], mode)
+
+  def testIncompatiblePSFLength(self):
+    psfs = [np.ones(12), np.ones(5)]
+    with self.assertRaisesRegex(ValueError, "All PSF's must have same shape"):
+      psf_utils.to_filter(psfs, "FROM_SAME")
+
+  def testIncompatiblePSFDimension(self):
+    psfs = [np.ones((12, 5)), np.ones((12, 5))]
+    with self.assertRaisesRegex(ValueError, "All PSF's must be 1D"):
+      psf_utils.to_filter(psfs, "FROM_SAME")
+
+
+if __name__ == "__main__":
+  unittest.main()
