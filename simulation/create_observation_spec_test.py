0a76c405ca568bf1f07dca7d27f7af4d0c0ec41f
Noahyt <NoahToyonaga@gmail.com>
2018-12-19 23:24:25 -0800

    Implement `create_observation_spec` file to write `ObservationSpec` to file.

---
+++ simulation/create_observation_spec_test.py
@@ -0,0 +1,51 @@
+"""Tests for `create_observation_spec`."""
+import json
+import unittest
+from unittest.mock import patch
+import os
+import shutil
+import sys
+import tempfile
+
+from simulation import defs
+from simulation import create_observation_spec
+
+
+class GenerateRecordsTest(unittest.TestCase):
+
+  def setUp(self):
+    # Create a test directory.
+    self.test_dir = tempfile.mkdtemp()
+
+  def tearDown(self):
+    # Remove the directory after the test
+    shutil.rmtree(self.test_dir)
+
+  def testCreateRecord(self):
+    angles=[0, 1., 2.]
+    frequencies=[3.e6, 4.e2, 5.e6]
+    grid_dimension=.5e-4
+    transducer_bandwidth=1.
+    numerical_aperture=2.
+    true_spec = defs.ObservationSpec(
+      angles, frequencies, grid_dimension, transducer_bandwidth,
+      numerical_aperture)
+
+    test_args = ["test_arg",
+      "-sd", self.test_dir,
+      "-n", 'test_name',
+      "-a", "{}".format(','.join(str(a) for a in angles)),
+      "-f", "{}".format(','.join(str(f) for f in frequencies)),
+      "-gd", "{}".format(grid_dimension),
+      "-tb", "{}".format(transducer_bandwidth),
+      "-na", "{}".format(numerical_aperture),
+    ]
+    with patch.object(sys, 'argv', test_args):
+      create_observation_spec.main()
+    file = os.path.join(self.test_dir, 'test_name')
+    with open(file) as f:
+      loaded_spec = defs.ObservationSpec(**json.load(f))
+    self.assertSequenceEqual(loaded_spec, true_spec)
+
+if __name__ == "__main__":
+  unittest.main()
