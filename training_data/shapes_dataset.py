a9df219f813e760e6080f8babbeb00d4d59429e0
Noahyt <NoahToyonaga@gmail.com>
2019-03-04 18:12:29 -0800

    Add generator methods and saving to `shapes_dataset`.

--- training_data/shapes_dataset.py
+++ training_data/shapes_dataset.py
@@ -1,15 +1,17 @@
 """Functions to generate dataset (in shards) for certain geometric shapes"""
 
-import numpy as np
 import math
-import random
-from skimage.draw import line_aa
+import os
+
+import numpy as np
 
 from simulation import response_functions
 
 from typing import List
 
-VALID_TYPES = ['CIRCLE', 'LINE']
+
+_CIRCLE = 'CIRCLE'
+VALID_TYPES = [_CIRCLE]
 
 
 def _circle(
@@ -96,118 +98,57 @@ def random_circles(
   return np.clip(box, a_max=max_intensity)
 
 
-def line_2d_endpoints(array_size: int, origin, grad: float):
-  """Returns endpoints of the 2d line passing through an origin with a given
-  gradient in an array.
-
-  Args:
-      array_size: size of each dimension of array.
-      origin: Tuple(int, int) indices of a point on the line.
-      grad: gradient at origin.
-
-  Returns:
-      Tuple(int, int, int, int) representing coordinates of left and right
-      endpoints in the form of (x_left, y_left, x_right, y_right).
-  """
-
-  x_left = y_left = x_right = y_right = 0
-
-  if grad == 0:
-      return 0, origin[1], array_size, origin[1]
-  elif grad > 0:
-      x_diff_left = max(-1 * origin[0] , -1 * float(origin[1]) / grad)
-      x_diff_right = min(array_size - 1 - origin[0], \
-          float(array_size - 1 - origin[1]) / grad)
-  else:
-      x_diff_left = max(-1 * origin[0], \
-          -1 * float(origin[1] - array_size + 1) / grad)
-      x_diff_right = min(array_size - 1 - origin[0], \
-          - 1 * float(origin[1]) / grad)
-
-  x_left = floor(origin[0] + x_diff_left)
-  y_left = ceil(origin[1] + x_diff_left * grad)
-  x_right = floor(origin[0] + x_diff_right)
-  y_right = ceil(origin[1] + x_diff_right * grad)
-
-  return x_left, y_left, x_right, y_right
-
-def line_fn(
-  physical_dim: float,
-  grid_size: float,
-  max_count: int,
-  sharpness: int = 100,
-  num_dim: int = 2,
-):
-  """Produces an array of lines.
-
-  Set cells are `1` while empty cells are `0`.
-
-  Increasing sharpness causes line to be thinner and more salient.
-
-  Args:
-    physical_dim: length of box in metres.
-    grid_size: grid size in metres.
-    max_count: maximum number of lines generated.
-    sharpness: anti-aliasing threshold for cell to be set.
-    num_dim: number of dimensions of box (only 2 for now).
-
-  Returns:
-    np.ndarray with total dimensions num_dim with each dimension having
-    size `physical_dim / grid_size`.
-  """
-
-  array_size = ceil(physical_dim / grid_size)
-  count = np.random.randint(1, max_count + 1)
-  box = np.zeros([array_size] * num_dim)
-
-  for _ in range(count):
-      origin = np.random.randint(0, array_size, num_dim)
-      grad = tan(random.uniform(-pi / 2, pi / 2))
-
-      x_left, y_left, x_right, y_right = line_2d_endpoints(array_size, origin, grad)
-
-      rr, cc, val = line_aa(x_left, y_left, x_right, y_right)
-      box[rr, cc] += val * 255
-
-  box = (box > sharpness).astype(int)
-
-  return box
-
-
-def make_dataset(
+def shape_generator(
   type: str,
-  count: int,
-  shard_size: int = 1000,
-  file_prefix: str,
-  directory: str,
   *args,
+  **kwargs,
 ):
-  """Writes data of a given type in a directory as .npy shards.
-
-  Each shard file contains a np.ndarray of np.ndarray. Each np.ndarray element
-  of the parent np.ndarray is a dataset of randomly generated shapes of `type`
-  where set cells are '1' and empty cells are '0'.
+  """Generator for shapes of a given type.
 
   Args:
-    type: shape to generate in data.
-    count: total number of datasets.
-    shard_size: number of datasets in a shard.
-    file_prefix: prefix to be appended before shard index.
-    directory: location to save.
-    args: parameters to pass into the corresponding type_fn (e.g. circle_fn
-          if type == 'circle' and line_fn if type == 'line').
+    type: Shape to generate in data.
+    args: Parameters to pass into the corresponding shape function.
+    kwargs: Same as `args`.
   """
-
-  shape_fn = None
-  if (type == 'circle'):
-      shape_fn = circle_fn
-  elif (type == 'line'):
-      shape_fn = line_fn
+  if (type == _CIRCLE):
+      shape_fn = random_circles
   else:
       raise ValueError("type must be in %s" % str(VALID_TYPES))
+  while True:
+    yield shape_fn(*args, **kwargs)
 
-  num_files = ceil(float(count) / shard_size)
-  for i in range(num_files):
-      output = [shape_fn(*args) for _ in range(shard_size)]
-      file_path = "%s/%s_%d_of_%d.npy" % (directory, file_prefix, i+1, num_files)
-      np.save(file_path, output)
+def save_dataset(
+  generator,
+  count,
+  shard_size,
+  save_directory,
+  file_prefix,
+):
+  """Saves datset of elements from generator.
+
+  This function builds and saves a dataset of scatterer distributions given
+  a generator which produces np.ndarrays contining scatterer distributions.
+
+  Each shard contains a single np.ndarrays with shape
+  `[shard_size] + generator_output_shape`. This implies that the generator must
+  return a consistent array size.
+
+  Args:
+    generator: Generator which returns np.ndarray objects.
+    count: Total number of distributions in dataset.
+    shard_size: Number of distributions per shard.
+    save_directory: Path to folder where data will be saved.
+    file_prefix: Optional prefix to add to filenames.
+
+  Raises:
+    ValueError if `save_directory` is not valid.
+  """
+  if not os.path.isdir(save_directory):
+    raise ValueError("`save_directory` must be a valid directory.")
+
+  file_count = int(math.ceil(float(count) / shard_size))
+  for shard in range(1, file_count + 1):
+    output = np.stack([next(generator) for _ in range(shard_size)], 0)
+    filename = "%s_%d_of_%d.npy" % (file_prefix, shard, file_count)
+    output_file = os.path.join(save_directory, filename)
+    np.save(output_file, output)
