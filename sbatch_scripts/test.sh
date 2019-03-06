b98437dd529cf8c6dafa20b04a4f1e6a221937c5
Noahyt <NoahToyonaga@gmail.com>
2019-03-06 14:48:27 -0800

    Simplify test script.

--- sbatch_scripts/test.sh
+++ sbatch_scripts/test.sh
@@ -5,51 +5,55 @@ sbatch_setup_commands="# Additional setup commands. \n"
 job_name=test
 directory=output
 
-
 ## JOB RUNTIME SPECIFICATIONS
 time='1:00'
 partition=normal
 gpu_count=0
 
+## GET INPUTS.
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
 ## OVERRIDE DEFAULTS WITH PASSED PARAMETERS
-while :; do
-    case $1 in
-        -h|-\?|--help)
-            Help.
-	    ;;
-        -n|--job_name)
-	    if [ ! -z "$2" ]; then
-            	job_name=$2
-            	shift
-	    else
-		echo 'ERROR: "--job_name" requires a non-empty option argument.'
-		exit 1
-	    fi
-	    ;;
-        -d|--directory)
-            if [ ! -z "$2" ]; then
-                directory=$2
-                shift
-            else
-                echo 'ERROR: "--directory" requires a non-empty option argument.'
-		exit 1
-            fi
-            ;;
-	-g|--gpu_count)
-	    if [ ! -z "$2" ]; then
-                gpu_count=$2
-                shift
-            else
-                gpu_count=1
-		echo '"gpu_count" set to 1 as no value was given.'
-            fi
-            ;;
-	-?*)
-	    die "WARNING: Unkown option: $1"
-	    ;;
-	*)
-	    break
-    esac
+case $key in
+    -h|-\?|--help)
+        Help.
+    ;;
+    -n|--job_name)
+    if [ ! -z "$2" ]; then
+            job_name=$2
+            shift
+    else
+    echo 'ERROR: "--job_name" requires a non-empty option argument.'
+    exit 1
+    fi
+    ;;
+    -d|--directory)
+        if [ ! -z "$2" ]; then
+            directory=$2
+            shift
+        else
+            echo 'ERROR: "--directory" requires a non-empty option argument.'
+    exit 1
+        fi
+        ;;
+    -g|--gpu_count)
+    if [ ! -z "$2" ]; then
+            gpu_count=$2
+            shift
+        else
+            gpu_count=1
+    echo '"gpu_count" set to 1 as no value was given.'
+        fi
+        ;;
+-?*)
+    die "WARNING: Unkown option: $1"
+    ;;
+*)
+    break
+esac
 done
 
 if [ $gpu_count -gt 0 ]; then
@@ -58,9 +62,9 @@ if [ $gpu_count -gt 0 ]; then
 fi
 
 ## SET UP JOB DIRECTORIES.
-if [ ! -d $output_directory ]; then
+if [ ! -d directory ]; then
   # If directory does not exist, then creates it.
-  mkdir -p $output_directory
+  mkdir -p $directory
 fi
 
 echo $sbatch_setup_commands
@@ -85,12 +89,10 @@ sbatch <<EOT
 # the appended 'sbatch_setup_commands'.
 #SBATCH --partition=${partition}
 #SBATCH --time=${time}
-#SBATCH --mem=60000
+#SBATCH --mem-per-cpu=4G
 
 $sbatch_setup_commands
 
 srun hostname
 srun sleep 60
-
 EOT
-
