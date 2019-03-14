1fb2e501b3e5660afb9a564ce61f7d6601e33309
Noahyt <NoahToyonaga@gmail.com>
2019-03-14 15:04:00 -0700

    Add `rotate_back` option to `simulate`.

--- simulation/simulate_test.py
+++ simulation/simulate_test.py
@@ -53,6 +53,33 @@ class SimulationTest(unittest.TestCase):
       descriptions, sim_test.psf_descriptions)
      ]
 
+  def testRotateBack(self):
+    angles = np.random.rand(5)
+    frequencies = np.random.rand(4) * 1e6
+    frequency_sigmas = np.random.rand(4) * 1e5
+    numerical_apertures = np.random.rand(4)
+    modes=[0, 1, 2, 3]
+    grid_unit=3.3e-5
+
+    descriptions = [
+      defs.PsfDescription(f, m, fs, na) for f, m, fs, na in zip(
+        frequencies, modes, frequency_sigmas, numerical_apertures
+      )
+    ]
+
+    sim_test = self._make_simulator(
+      angles=angles,
+      grid_unit=grid_unit,
+      psf_descriptions=descriptions,
+      psf_transverse_length=2.e-4,
+      psf_axial_length=1.e-4,
+    )
+    np.testing.assert_equal(False, sim_test.rotate_back)
+
+    sim_test.rotate_back = True
+
+    np.testing.assert_equal(True, sim_test.rotate_back)
+
   def testSetAngles(self):
     angles = np.random.rand(5)
     frequencies = np.random.rand(4) * 1e6
