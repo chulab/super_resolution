f8872445bb6837d11db4eb62c66d102381ef294e
Noahyt <NoahToyonaga@gmail.com>
2019-03-14 17:43:37 -0700

    Add import for `viz` module.
    Required to import `py-matplotlib`.

--- sbatch_scripts/generate_distributions.sh
+++ sbatch_scripts/generate_distributions.sh
@@ -122,6 +122,8 @@ $sbatch_setup_commands
 
 ml py-numpy/1.14.3_py36
 ml py-scipy/1.1.0_py36
+
+ml viz
 ml py-matplotlib/2.2.2_py36
 
 python3.6 $PI_HOME/super_resolution/super_resolution/training_data/generate_scatterer_dataset.py \
