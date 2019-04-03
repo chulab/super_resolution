dd72ada0020c632682d21ffabd274e6c9bc3c970
Noahyt <NoahToyonaga@gmail.com>
2019-04-03 00:25:25 -0700

    Add main to `test_input_pipeline`.

--- preprocessing/test_input_pipeline.py
+++ preprocessing/test_input_pipeline.py
@@ -1,12 +1,21 @@
 """Verification for input pipeline by saving some data."""
-
+import argparse
 import logging
 import time
 import os
+import sys
 
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from preprocessing import input
+from simulation import create_observation_spec
+from trainer import basic_model as model
+from utils import logging_utils
+
 
 def save_input(
     dataset: tf.data.Dataset,
@@ -51,3 +60,37 @@ def save_input(
     fig.savefig(output_file)
 
     del fig
+
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+
+  args, _ = parser.parse_known_args()
+
+  return args
+
+def main():
+  logging_utils.set_up_logging()
+  observation_spec = create_observation_spec.load_observation_spec(
+    "/Users/noah/Documents/CHU/super_resolution/super_resolution/utils/test_data/test_observation_spec.json",
+    False
+  )
+  parse_fns = model.input_fns_(
+    example_shape=[501,501],
+    observation_spec=observation_spec
+  )
+  dataset_directory = "/Users/noah/Documents/CHU/super_resolution/super_resolution/utils/test_data"
+  dataset = input.input_fn(
+    dataset_directory=dataset_directory,
+    parse_fns=parse_fns,
+    parallel_calls=[1] * len(parse_fns)
+  )
+  save_input(
+    dataset=dataset,
+    dataset_name="train_dataset",
+    output_dir="/Users/noah/Documents/CHU/super_resolution/super_resolution/preprocessing/input_test",
+    iteration_count=0,
+  )
+
+if __name__=="__main__":
+  main()
