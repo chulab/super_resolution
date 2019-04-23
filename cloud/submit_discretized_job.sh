6bd3cc2e2c756f3b10614df9ee3542b42d056f7f
Noahyt <NoahToyonaga@gmail.com>
2019-04-22 17:44:47 -0700

    Training scripts.

--- cloud/submit_discretized_job.sh
+++ cloud/submit_discretized_job.sh
@@ -14,6 +14,9 @@ OUTPUT_PATH=$JOB_DIR
 
 MODULE_NAME=trainer.train_discretized_model
 
+CLOUD_DATA_TRAIN='gs://chu_super_resolution_data/simulation/circle_3_18_envelope/train'
+CLOUD_DATA_EVAL='gs://chu_super_resolution_data/simulation/circle_3_18_envelope/eval'
+CLOUD_OBSERVATION_SPEC='gs://chu_super_resolution_data/simulation/circle_3_18_envelope/observation_spec.json'
 
 # Train on Cloud.
 gcloud ml-engine jobs submit training $JOB_NAME \
@@ -38,20 +41,20 @@ gcloud ml-engine jobs submit training $JOB_NAME \
     --log_step_count 20 \
         --hparams \
 learning_rate=.0001,\
-conv_blocks=4,\
+conv_blocks=5,\
 spatial_blocks=5,\
 spatial_kernel_size=5,\
-spatial_scales=[1,2,4,8,16],\
+spatial_scales=[1,2,4,8],\
 filters_per_scale=16,\
 residual_blocks=0,\
 residual_channels=64,\
 residual_kernel_size=3,\
 residual_scale=.1,\
-bit_depth=2,\
+bit_depth=4,\
 observation_pool_downsample=8,\
 distribution_pool_downsample=30, \
-    --warm_start_from_dir 'gs://chu_super_resolution_experiment/discretized_12_04_2019_12_10_05'\
 
+#    --warm_start_from_dir 'gs://chu_super_resolution_experiment/discretized_12_04_2019_12_10_05'\
 
 #
 #LOCAL_OUTPUT='trainer/test_output'
